07305f25c1df80c8c57e380ae10c4fe3d07cd0d7
hassannh <hassannouhi91@gmail.com>
2022-09-08 09:53:56 -0400

    if pos if negative

---
+++ if-negative-if-pos.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int   main()
+{
+    int input, i=0;
+    while(i=0) {
+
+    printf("enter your number:");
+  
+        scanf("%d", &input);
+        if(input < 0){
+           printf("your number is negative!!!");
+	}else if (input > 0){
+	
+		printf("your number is positive!!!");
+	}else {
+	
+		printf("invalid number");
+	}	
+		
+	
+
+    }
+
+
+
+    return 0;
+
